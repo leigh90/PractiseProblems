5987b12aea59766ac252c74716d149c5e74cabf7
leigh90 <zawadikibathi@gmail.com>
2021-02-24 04:03:58 +0300

    one liner hehe

---
+++ Paleendrome/palindrome.py
@@ -0,0 +1,25 @@
+# take in a number
+# convert to string
+# reverse the string
+# check if it is equal to the original string
+# return true or false
+
+
+def ispalindrome(num):
+    # new_string = str(num)
+    # print(new_string)
+    # reversed_string = new_string[::-1]
+    # print(reversed_string)
+    # if reversed_string == new_string:
+    #     print('We have a palindrome')
+    # else:
+    #     print('False')
+    # OR
+    result = str(num) == str(num)[::-1]
+    print(result)
+
+#     ;D ;D ;D ;D ;D so much to learn
+
+
+
+ispalindrome(123)
