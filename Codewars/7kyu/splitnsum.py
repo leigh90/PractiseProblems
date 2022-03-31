56fdbe4c3a398dbe684e46c3ebd0c5a5d83f6358
leigh90 <ashley@angaza.com>
2022-03-31 09:49:18 +0300

    Problem 23

---
+++ Codewars/7kyu/splitnsum.py
@@ -0,0 +1,12 @@
+def sum_digits(number):
+    if number < 0:
+        number *= -1
+    num_list = list(str(number))
+    sumof = sum([int(num) for num in num_list ])
+    print(sumof)
+
+sum_digits(-32)
+
+a = 10
+b = 5
+print(eval('a * b'))
